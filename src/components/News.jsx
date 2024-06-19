cac02969717470579690e5492292c185c3ae2fb6
Rahul <rsc120@nist.edu>
2024-06-19 22:45:51 +0530

    Updated

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,23 +2,38 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div className="container my-4">
-        <div className="row">
-            <div className="col md-4">
-                <NewsItem title="mytitle" description="mydescription" imageUrl="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTKqIg3pZGnGVuDbO7piYwe2EBzDMOcMohDv5sIWQ-tnD7ruRla"></NewsItem>
+    constructor() {
+        super();
+        console.log("Hello I am a constructor");
+        this.state = {
+            articles: [],
+            loading: false
+        }
+    }
+    async componentDidMount(){
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=527270c2589c4957850b4bf25c8cef15"
+        let data= await fetch(url);
+        let parsedData= await data.json();
+        this.setState({articles:parsedData.articles})
+    }
+    render() {
+        return (
+            <div className="container my-3">
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return (
+                            <div key={element.url} className="col-md-4">
+                                <NewsItem title={element.title?element.title:""} description={element.description?element.description:""}
+                                    imageUrl={element.urlToImage?element.urlToImage:"https://static.vecteezy.com/system/resources/thumbnails/004/216/831/original/3d-world-news-background-loop-free-video.jpg"}
+                                    newsUrl={element.url}
+                                ></NewsItem>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-            <div className="col md-4">
-                <NewsItem title="mytitle" description="mydescription"></NewsItem>
-            </div>
-            <div className="col md-4">
-                <NewsItem title="mytitle" description="mydescription"></NewsItem>
-            </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default News
