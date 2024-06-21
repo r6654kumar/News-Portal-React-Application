700f5bdbf7512de6745cc5ab6494366f1fda5af0
Rahul <rsc120@nist.edu>
2024-06-21 14:54:29 +0530

    Spinner Added

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import loading from './loading.gif'
 export class Spinner extends Component {
   render() {
     return (
-      <div className='text-center'>
+      <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
         <img src={loading} alt="loading" />
       </div>
     )
